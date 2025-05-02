b556c8552b16cd57389c4a054da6398adac68102
DhavalCoder <149752141+DhavalCoder@users.noreply.github.com>
2025-05-03 00:16:38 +0530

    Update delayed_dut.v

--- hdl/delayed_dut.v
+++ hdl/delayed_dut.v
@@ -47,6 +47,11 @@ module dut(CLK,
 	   read_en,
 	   read_data,
 	   read_rdy);
+	initial begin
+		$dumpfile("hdl/delayed_dut.vcd");   // <-- this path matches what your GitHub action expects
+  $dumpvars(0, dut);
+end
+
   input  CLK;
   input  RST_N;
 
